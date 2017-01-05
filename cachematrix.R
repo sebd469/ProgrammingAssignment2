dfe61d9c8355fb03a7652cbf13f6576469165da5
Seb <sebd469@gmail.com>
2017-01-05 20:06:26 +0000

    Matrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Create an R object to store a matrix x and its inverse invm
+makeMatrix <- function(x = matrix()) {
+  invm <- NULL
+  set <- function(y) {
+    x <<- y
+    invm <<- NULL
+  }
+  get <- function() x
+  setinv <- function(i) invm <<- i
+  getinv <- function() invm
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Retrieve the inverse matrix from cached value stored in makeMatrix
+# This requires an argument returned by makeMatrix
+# Call makeMAtrix FIRST
+cacheInv <- function(x, ...) {
+  
+  invm <- x$getinv()
+  if(!is.null(invm)) {
+    message("getting cached data")
+   
+    return(invm)
+  }
+  data <- x$get()
+  invm <- solve(data, ...)
+  x$setinv(invm)
+  
+  invm
 }
+
+#to run:
+# x=matrix(1:4,2,2)
+# y=makeMatrix(x)
+# check using y$get()
+# cacheInv(y)  to be run twice once calc. then cached!
